371da4b4c2138c926379b568066c81964dc714e5
Faisal-P9 <faisal@planetnine.com>
2024-05-14 18:12:57 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,276 @@
+
+var apto0 = document.getElementById('apto0');
+var apto0_active = document.getElementById('apto0_active');
+var inter0 = document.getElementById('inter0');
+var inter0_active = document.getElementById('inter0_active');
+var swipe0 = document.getElementById('swipe0');
+var swipe0_active = document.getElementById('swipe0_active');
+
+var apto1 = document.getElementById('apto1');
+var apto1_active = document.getElementById('apto1_active');
+var inter1 = document.getElementById('inter1');
+var inter1_active = document.getElementById('inter1_active');
+var swipe1 = document.getElementById('swipe1');
+var swipe1_active = document.getElementById('swipe1_active');
+
+var apto2 = document.getElementById('apto2');
+var apto2_active = document.getElementById('apto2_active');
+var inter2 = document.getElementById('inter2');
+var inter2_active = document.getElementById('inter2_active');
+var swipe2 = document.getElementById('swipe2');
+var swipe2_active = document.getElementById('swipe2_active');
+
+var apto3 = document.getElementById('apto3');
+var apto3_active = document.getElementById('apto3_active');
+var inter3 = document.getElementById('inter3');
+var inter3_active = document.getElementById('inter3_active');
+var swipe3 = document.getElementById('swipe3');
+var swipe3_active = document.getElementById('swipe3_active');
+
+var apto4 = document.getElementById('apto4');
+var apto4_active = document.getElementById('apto4_active');
+var inter4 = document.getElementById('inter4');
+var inter4_active = document.getElementById('inter4_active');
+var swipe4 = document.getElementById('swipe4');
+var swipe4_active = document.getElementById('swipe4_active');
+
+var apto5 = document.getElementById('apto5');
+var apto5_active = document.getElementById('apto5_active');
+var inter5 = document.getElementById('inter5');
+var inter5_active = document.getElementById('inter5_active');
+var swipe5 = document.getElementById('swipe5');
+var swipe5_active = document.getElementById('swipe5_active');
+
+
+
+
+
+// BTN #0
+
+apto0.addEventListener('click', function() {
+    apto0.style.display = 'none';
+    apto0_active.style.display = 'block';
+
+    inter0.style.display = 'block';
+    inter0_active.style.display = 'none';
+
+    swipe0.style.display = 'block';
+    swipe0_active.style.display = 'none';
+});
+
+
+inter0.addEventListener('click', function() {
+    apto0.style.display = 'block';
+    apto0_active.style.display = 'none';
+
+    inter0.style.display = 'none';
+    inter0_active.style.display = 'block';
+
+    swipe0.style.display = 'block';
+    swipe0_active.style.display = 'none';
+});
+
+
+swipe0.addEventListener('click', function() {
+    apto0.style.display = 'block';
+    apto0_active.style.display = 'none';
+
+    inter0.style.display = 'block';
+    inter0_active.style.display = 'none';
+
+    swipe0.style.display = 'none';
+    swipe0_active.style.display = 'block';
+});
+
+
+// BTN #1
+
+apto1.addEventListener('click', function() {
+    apto1.style.display = 'none';
+    apto1_active.style.display = 'block';
+
+    inter1.style.display = 'block';
+    inter1_active.style.display = 'none';
+
+    swipe1.style.display = 'block';
+    swipe1_active.style.display = 'none';
+});
+
+
+inter1.addEventListener('click', function() {
+    apto1.style.display = 'block';
+    apto1_active.style.display = 'none';
+
+    inter1.style.display = 'none';
+    inter1_active.style.display = 'block';
+
+    swipe1.style.display = 'block';
+    swipe1_active.style.display = 'none';
+});
+
+
+swipe1.addEventListener('click', function() {
+    apto1.style.display = 'block';
+    apto1_active.style.display = 'none';
+
+    inter1.style.display = 'block';
+    inter1_active.style.display = 'none';
+
+    swipe1.style.display = 'none';
+    swipe1_active.style.display = 'block';
+});
+
+
+// BTN #2
+
+apto2.addEventListener('click', function() {
+    apto2.style.display = 'none';
+    apto2_active.style.display = 'block';
+
+    inter2.style.display = 'block';
+    inter2_active.style.display = 'none';
+
+    swipe2.style.display = 'block';
+    swipe2_active.style.display = 'none';
+});
+
+
+inter2.addEventListener('click', function() {
+    apto2.style.display = 'block';
+    apto2_active.style.display = 'none';
+
+    inter2.style.display = 'none';
+    inter2_active.style.display = 'block';
+
+    swipe2.style.display = 'block';
+    swipe2_active.style.display = 'none';
+});
+
+
+swipe2.addEventListener('click', function() {
+    apto2.style.display = 'block';
+    apto2_active.style.display = 'none';
+
+    inter2.style.display = 'block';
+    inter2_active.style.display = 'none';
+
+    swipe2.style.display = 'none';
+    swipe2_active.style.display = 'block';
+});
+
+
+// BTN #3
+
+apto3.addEventListener('click', function() {
+    apto3.style.display = 'none';
+    apto3_active.style.display = 'block';
+
+    inter3.style.display = 'block';
+    inter3_active.style.display = 'none';
+
+    swipe3.style.display = 'block';
+    swipe3_active.style.display = 'none';
+});
+
+
+inter3.addEventListener('click', function() {
+    apto3.style.display = 'block';
+    apto3_active.style.display = 'none';
+
+    inter3.style.display = 'none';
+    inter3_active.style.display = 'block';
+
+    swipe3.style.display = 'block';
+    swipe3_active.style.display = 'none';
+});
+
+
+swipe3.addEventListener('click', function() {
+    apto3.style.display = 'block';
+    apto3_active.style.display = 'none';
+
+    inter3.style.display = 'block';
+    inter3_active.style.display = 'none';
+
+    swipe3.style.display = 'none';
+    swipe3_active.style.display = 'block';
+});
+
+
+
+// BTN #4
+
+apto4.addEventListener('click', function() {
+    apto4.style.display = 'none';
+    apto4_active.style.display = 'block';
+
+    inter4.style.display = 'block';
+    inter4_active.style.display = 'none';
+
+    swipe4.style.display = 'block';
+    swipe4_active.style.display = 'none';
+});
+
+
+inter4.addEventListener('click', function() {
+    apto4.style.display = 'block';
+    apto4_active.style.display = 'none';
+
+    inter4.style.display = 'none';
+    inter4_active.style.display = 'block';
+
+    swipe4.style.display = 'block';
+    swipe4_active.style.display = 'none';
+});
+
+
+swipe4.addEventListener('click', function() {
+    apto4.style.display = 'block';
+    apto4_active.style.display = 'none';
+
+    inter4.style.display = 'block';
+    inter4_active.style.display = 'none';
+
+    swipe4.style.display = 'none';
+    swipe4_active.style.display = 'block';
+});
+
+
+
+
+// BTN #5
+
+apto5.addEventListener('click', function() {
+    apto5.style.display = 'none';
+    apto5_active.style.display = 'block';
+
+    inter5.style.display = 'block';
+    inter5_active.style.display = 'none';
+
+    swipe5.style.display = 'block';
+    swipe5_active.style.display = 'none';
+});
+
+
+inter5.addEventListener('click', function() {
+    apto5.style.display = 'block';
+    apto5_active.style.display = 'none';
+
+    inter5.style.display = 'none';
+    inter5_active.style.display = 'block';
+
+    swipe5.style.display = 'block';
+    swipe5_active.style.display = 'none';
+});
+
+
+swipe5.addEventListener('click', function() {
+    apto5.style.display = 'block';
+    apto5_active.style.display = 'none';
+
+    inter5.style.display = 'block';
+    inter5_active.style.display = 'none';
+
+    swipe5.style.display = 'none';
+    swipe5_active.style.display = 'block';
+});
